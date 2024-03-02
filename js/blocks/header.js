291be15e37c5be8228307cba07939bc6e1e2fe74
Luca Nerlich <luca.nerlich@gmail.com>
2024-03-02 22:17:19 +0100

    Updated CSS for teaser-cta, sections, and header
    Enhanced UI by applying changes to the stylesheets of various components. This includes importing teaser-cta in main.css, adding margin to sections in style.css, adjusting overlap effect in home.css, and redefining teaser-cta and hero components. Header was also adjusted for sticky positioning. Changes aim to improve depth perception and overall user experience.

--- js/blocks/header.js
+++ js/blocks/header.js
@@ -13,6 +13,7 @@ function createHeader(header) {
                 <li><a href="/pages/category.html?category=men">Men</a></li>
                 <li><a href="/pages/category.html?category=electronics">Electronics</a></li>
                 <li><a href="/pages/category.html?category=jewellery">Jewellery</a></li>
+                <li><a href="/pages/category.html?category=men&category=women&category=electronics&category=jewellery">All</a></li>
             </ul>
         </li>
         <li><a href="/pages/basket.html">BASKET</a></li>
--- js/pages/category.js
+++ js/pages/category.js
@@ -2,12 +2,11 @@ import {loadData} from "../api.js";
 import {capitalize} from "../util.js";
 
 async function createCategory(main) {
-    const category = new URLSearchParams(window.location.search).get("category");
+    const category = new URLSearchParams(window.location.search).getAll("category");
     if (!category) {
         console.error("No category found for product page.");
     }
-
-    document.title = capitalize(category);
+    document.title = category.length > 1 ? "Categories" : capitalize(category[0]);
 
     const categories = await loadData('/assets/categories.json');
     console.log("categories", categories);
