7299ac9b35146d03ad9ac8a34b3b5b26d033edf2
Luca Nerlich <luca.nerlich@gmail.com>
2024-03-02 21:45:32 +0100

    Introduce loadData function to fetch JSON data
    A new method `loadData` is introduced in `js/api.js`, which fetches data asynchronously from a given URL and returns a promise. This function is now being utilized in `product.js` and `category.js` to load product and category data respectively. If no product or category is found, error handling is implemented to handle these cases gracefully.

--- js/pages/category.js
+++ js/pages/category.js
@@ -1,9 +1,13 @@
+import {loadData} from "../api.js";
+
 async function createCategory(main) {
     const category = new URLSearchParams(window.location.search).get("category");
     if (!category) {
         console.error("No category found for product page.")
     }
 
+    const categories = await loadData('/assets/categories.json');
+    console.log("categories", categories);
 }
 
 createCategory(document.getElementById("category-main")).then(r => {
--- js/pages/product.js
+++ js/pages/product.js
@@ -1,8 +1,13 @@
+import {loadData} from "../api.js";
+
 async function createProduct(main) {
     const product = new URLSearchParams(window.location.search).get("product");
     if (!product) {
         console.error("No product found for product page.")
     }
+
+    const products = await loadData('/assets/products.json');
+    console.log("products", products);
 }
 
 createProduct(document.getElementById("product-main")).then(r => {
