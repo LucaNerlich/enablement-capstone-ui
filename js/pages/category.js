0f6ab7b6fe53053e0e68e4bcf55ac60b233b5d15
Luca Nerlich <luca.nerlich@gmail.com>
2024-03-02 22:36:52 +0100

    Refactor product and category data structure and update category.js
    Completely restructured products.json and categories.json file by adding id, title, and image fields to each product and adding a title and products array to each category. This new data structure now allows products to be categorized by their respective ids. With these changes, the category.js file was also adjusted to efficiently handle the new data layout.

--- js/pages/category.js
+++ js/pages/category.js
@@ -6,10 +6,23 @@ async function createCategory(main) {
     if (!category) {
         console.error("No category found for product page.");
     }
-    document.title = category.length > 1 ? "Categories" : capitalize(category[0]);
+    const isSingle = category.length === 1;
+    document.title = !isSingle ? "Categories" : capitalize(category[0]);
 
     const categories = await loadData('/assets/categories.json');
-    console.log("categories", categories);
+    const categoryData = isSingle ? {[category]: categories[category]} : categories;
+
+    // loop over categories and return all aggreagted product ids
+    const productIds = Object.values(categoryData).reduce((acc, category) => {
+        if (category.products && Array.isArray(category.products)) {
+            return acc.concat(category.products);
+        } else {
+            return acc;
+        }
+    }, []);
+    const products = await loadData('/assets/products.json');
+    const matchingProducts = products.filter(item => productIds.includes(item.id));
+    console.log("matchingProducts", matchingProducts);
 }
 
 createCategory(document.getElementById("category-main")).then(r => {
