fb985584a2466b07fe6305605ede1f8caf17e0fe
Luca Nerlich <luca.nerlich@googlemail.com>
2024-03-08 13:30:09 +0100

    Refactor product listing and improve basket sum rounding
    Switched product listing from a list to a table format, cleaned up basket.js by incorporating resetBasketTable() function for better readability. Also, fixed a bug in the basket sum where dollar amounts were not correctly rounded to the nearest two decimal places.

--- js/pages/basket.js
+++ js/pages/basket.js
@@ -4,7 +4,6 @@ import {shortenText} from "../util.js";
 
 async function createBasket(main) {
     document.title = "Basket";
-    console.log("main", main);
 
     main.querySelector('#clear-basket')?.addEventListener('click', () => {
         clearBasket();
@@ -16,17 +15,22 @@ async function createBasket(main) {
 
     function refreshBasketList() {
         const basketProductList = main.querySelector('#basket-product-list');
-        basketProductList.innerHTML = "";
+        // Reset table
+        resetBasketTable(basketProductList);
+
+        // Recreate table
         const basketSumElement = main.querySelector('#basket-sum')?.querySelector('span');
         let basketSum = 0;
         const basketCount = loadBasketCount();
         basketCount.forEach(product => {
-            const productListItem = document.createElement('li');
+            const productListItem = document.createElement('tr');
             const matchedProduct = getProductById(product.id, products);
             const itemSum = matchedProduct.price * product.count;
             basketSum += itemSum;
             productListItem.innerHTML = `
-                <span>${product.count}x <strong>${shortenText(matchedProduct.title, 25)}</strong> | $${itemSum}</span>
+                <td>${product.count}</td>
+                <td><strong>${shortenText(matchedProduct.title, 25)}</strong></td>
+                <td>| $${itemSum}</td>
             `
             basketProductList.appendChild(productListItem);
         })
@@ -34,6 +38,14 @@ async function createBasket(main) {
         basketSumElement.innerText = "$" + Math.round(basketSum * 100) / 100
     }
 
+    function resetBasketTable(basketProductList) {
+        const trElements = basketProductList.querySelectorAll("tr");
+        trElements.forEach(tr => {
+            if (!tr.querySelector("th")) {
+                tr.parentNode.removeChild(tr);
+            }
+        });
+    }
 
     function getProductById(productId, products) {
         return products.find(p => p.id === productId);
