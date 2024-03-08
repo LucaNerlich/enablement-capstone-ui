e7e639ba279643c12f39ed200f1db1ddde896c2f
Luca Nerlich <luca.nerlich@googlemail.com>
2024-03-08 14:30:01 +0100

    Refactor header component for mobile responsiveness
    The header component was updated to include a hamburger menu for mobile and tablet displays. JavaScript functionality has been added to toggle menu visibility. The associated CSS was altered to accommodate these changes, particularly within media queries for different display sizes.

--- js/blocks/header.js
+++ js/blocks/header.js
@@ -6,28 +6,33 @@ import {updateBadge} from "../basket.js";
  */
 async function createHeader(header) {
     header.innerHTML = `
-      <ul>
-        <li class="logo-name"><a href="/">VENIA</a></li>
-        <li>
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/pages/category.html?category=women">Women</a></li>
-                <li><a href="/pages/category.html?category=men">Men</a></li>
-                <li><a href="/pages/category.html?category=electronics">Electronics</a></li>
-                <li><a href="/pages/category.html?category=jewelery">Jewelery</a></li>
-                <li><a href="/pages/category.html">All</a></li>
-            </ul>
-        </li>
-        <li id="basket-badge">
-            <a href="/pages/basket.html">
-                <button>
-                    BASKET
-                </button>
-            </a>
-        </li>
-    </ul>
+    <div class="logo"><a href="/">VENIA</a></div>
+    <div class="menu-container">
+      <div class="hamburger-menu">
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+      </div>
+      <nav class="mobile-nav">
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/pages/category.html?category=women">Women</a></li>
+          <li><a href="/pages/category.html?category=men">Men</a></li>
+          <li><a href="/pages/category.html?category=electronics">Electronics</a></li>
+          <li><a href="/pages/category.html?category=jewelery">Jewelery</a></li>
+          <li><a href="/pages/category.html">All</a></li>
+          <li id="basket-badge"> <a href="/pages/basket.html"> <button> BASKET </button> </a> </li>
+        </ul>
+      </nav>
+    </div>
   `;
 
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const mobileNav = document.querySelector('.mobile-nav');
+
+    hamburgerMenu.addEventListener('click', function () {
+        mobileNav.style.display = mobileNav.style.display === 'block' ? 'none' : 'block';
+    });
     updateBadge();
 }
 
