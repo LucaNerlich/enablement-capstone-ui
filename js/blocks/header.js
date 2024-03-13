ee743a59cb66ba744d7a9fcd6586c8ce74fa0071
Luca Nerlich <luca.nerlich@googlemail.com>
2024-03-13 16:50:56 +0100

    wip mobile nav

--- js/blocks/header.js
+++ js/blocks/header.js
@@ -6,34 +6,39 @@ import {updateBadge} from "../basket.js";
  */
 async function createHeader(header) {
     header.innerHTML = `
-  <div class="logo">
-    <a href="/"><span>V</span>ENIA</a>
-  </div>
-  <nav class="mobile-nav">
-    <ul>
-      <li><a href="/"><strong>Home</strong></a></li>
-      <li><a href="/pages/category.html?category=women">Women</a></li>
-      <li><a href="/pages/category.html?category=men">Men</a></li>
-      <li><a href="/pages/category.html?category=electronics">Electronics</a></li>
-      <li><a href="/pages/category.html?category=jewelery">Jewelery</a></li>
-      <li><a href="/pages/category.html">All</a></li>
-    </ul>
-  </nav>
-  <a id="basket-badge" href="/pages/basket.html">
-    <button>BASKET</button>
-  </a>
-  <div class="hamburger-menu">
-    <div class="bar"></div>
-    <div class="bar"></div>
-    <div class="bar"></div>
-  </div>
+<div class="logo">
+  <a href="/"><span>V</span>ENIA</a>
+</div>
+<nav>
+  <ul>
+    <li><a href="/"><strong>Home</strong></a></li>
+    <li><a href="/pages/category.html?category=women">Women</a></li>
+    <li><a href="/pages/category.html?category=men">Men</a></li>
+    <li><a href="/pages/category.html?category=electronics">Electronics</a></li>
+    <li><a href="/pages/category.html?category=jewelery">Jewelery</a></li>
+    <li><a href="/pages/category.html">All</a></li>
+    <li>
+      <a class="basket-badge" href="/pages/basket.html">
+        <button>BASKET</button>
+      </a>
+    </li>
+  </ul>
+</nav>
+<a class="basket-badge" href="/pages/basket.html">
+  <button>BASKET</button>
+</a>
+<div class="hamburger-menu">
+  <div class="bar"></div>
+  <div class="bar"></div>
+  <div class="bar"></div>
+</div>
   `;
 
     const hamburgerMenu = document.querySelector('.hamburger-menu');
-    const mobileNav = document.querySelector('.mobile-nav');
+    const nav = document.querySelector('nav');
 
     hamburgerMenu.addEventListener('click', function () {
-        mobileNav.style.display = mobileNav.style.display === 'block' ? 'none' : 'block';
+        nav.style.display = nav.style.display === 'block' ? 'none' : 'block';
     });
     updateBadge();
 }
