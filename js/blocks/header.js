6726f60c8bd3ade2929e1023a8decd2740008121
Luca Nerlich <luca.nerlich@googlemail.com>
2024-03-13 16:30:29 +0100

    desktop nav updates

--- js/blocks/header.js
+++ js/blocks/header.js
@@ -6,25 +6,27 @@ import {updateBadge} from "../basket.js";
  */
 async function createHeader(header) {
     header.innerHTML = `
-    <div class="logo"><a href="/">VENIA</a></div>
-    <div class="menu-container">
-      <div class="hamburger-menu">
-        <div class="bar"></div>
-        <div class="bar"></div>
-        <div class="bar"></div>
-      </div>
-      <nav class="mobile-nav">
-        <ul>
-          <li><a href="/"><strong>Home</strong></a></li>
-          <li><a href="/pages/category.html?category=women">Women</a></li>
-          <li><a href="/pages/category.html?category=men">Men</a></li>
-          <li><a href="/pages/category.html?category=electronics">Electronics</a></li>
-          <li><a href="/pages/category.html?category=jewelery">Jewelery</a></li>
-          <li><a href="/pages/category.html">All</a></li>
-          <li id="basket-badge"> <a href="/pages/basket.html"> <button> BASKET </button> </a> </li>
-        </ul>
-      </nav>
-    </div>
+  <div class="logo">
+    <a href="/"><span>V</span>ENIA</a>
+  </div>
+  <nav class="mobile-nav">
+    <ul>
+      <li><a href="/"><strong>Home</strong></a></li>
+      <li><a href="/pages/category.html?category=women">Women</a></li>
+      <li><a href="/pages/category.html?category=men">Men</a></li>
+      <li><a href="/pages/category.html?category=electronics">Electronics</a></li>
+      <li><a href="/pages/category.html?category=jewelery">Jewelery</a></li>
+      <li><a href="/pages/category.html">All</a></li>
+    </ul>
+  </nav>
+  <a id="basket-badge" href="/pages/basket.html">
+    <button>BASKET</button>
+  </a>
+  <div class="hamburger-menu">
+    <div class="bar"></div>
+    <div class="bar"></div>
+    <div class="bar"></div>
+  </div>
   `;
 
     const hamburgerMenu = document.querySelector('.hamburger-menu');
