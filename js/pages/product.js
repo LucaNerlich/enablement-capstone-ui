7bc51b717528c65b996ee6133dad700792e3397c
Luca Nerlich <luca.nerlich@googlemail.com>
2024-03-15 08:59:21 +0100

    filter products by given id

--- js/pages/product.js
+++ js/pages/product.js
@@ -1,12 +1,17 @@
-import {capitalize} from "../util.js";
+import {capitalize, shortenText} from "../util.js";
+import {loadData} from "../api.js";
 
 async function createProduct(main) {
-    const product = new URLSearchParams(window.location.search).get("product");
-    if (!product) {
-        console.error("No product found for product page.")
+    const productId = Number(new URLSearchParams(window.location.search).get("pid"));
+    if (!productId) {
+        console.error("No productId found for productId page.")
     }
 
-    document.title = capitalize(product);
+    const url = "/assets/products/all.json"
+    const products = await loadData(url);
+    const matchedProduct = products.find((product) => product.id === productId);
+
+    document.title = capitalize(shortenText(matchedProduct?.title, 25));
 }
 
 createProduct(document.getElementById("product-main")).then(r => {
